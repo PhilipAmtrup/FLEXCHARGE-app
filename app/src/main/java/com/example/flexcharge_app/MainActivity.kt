885883eaaca214b135c2ec66af0bdaf81c5f3579
Mo Zaabat <mzaabat@hotmail.com>
2024-10-31 18:52:04 +0100

    Problem200 side stortset færdig, mangler billede. Forsiden med menubar skal ændres

--- app/src/main/java/com/example/flexcharge_app/MainActivity.kt
+++ app/src/main/java/com/example/flexcharge_app/MainActivity.kt
@@ -3,6 +3,16 @@ package com.example.flexcharge_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.flexcharge_app.ui.Forside.ForsideScreen
 import com.example.flexcharge_app.ui.theme.FLEXCHARGEappTheme
 
@@ -17,4 +27,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+/*
+@Composable
+fun FlexChargeApp() {
+    FLEXCHARGEappTheme {
+
+        Scaffold(
+            topBar = {
+
+            }
+        )
+
+    }
+
+}
+
+
+fun TopBar() {
+
+}
+*/
 
--- app/src/main/java/com/example/flexcharge_app/ui/Problem200/problem200.kt
+++ app/src/main/java/com/example/flexcharge_app/ui/Problem200/problem200.kt
@@ -8,7 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,8 +24,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,7 +45,7 @@ fun Problem200() {
             .background(Color(0xFFF5F5F5))
             .padding(vertical = 16.dp)
     ) {
-        TopBar("Problemer med ladestation")
+        TopBar("Problemer med ladestationen")
 
         Text(
             text = "Har du problemer med din ladestation?",
@@ -46,25 +56,61 @@ fun Problem200() {
                 .align(Alignment.CenterHorizontally)
         )
 
+        Text(
+            text = "Fortæl os nærmere om problemerne ved ladestanderen",
+            style = MaterialTheme.typography.bodyMedium.copy(
+                fontSize = 22.sp,
+                fontFamily = FontFamily.Default
+            ),
+            color = Color.DarkGray,
+            modifier = Modifier
+                .padding(top = 13.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+
+
+
+
+
+
         Spacer(modifier = Modifier.height(16.dp))
 
-
-
-
-
-        Spacer(modifier = Modifier.height(16.dp))
-
+        ButtonRow("Elbilen melder fejl, hvad gør jeg?", horizontalPadding = 0.dp)
+        ButtonRow("Ladestationen lyser rød, hvad gør jeg?", horizontalPadding = 0.dp)
+        ButtonRow("Jeg kan ikke få ladekablet ud ladestationen...", horizontalPadding = 0.dp)
+        ButtonRow("Jeg har et andet problem...", horizontalPadding = 0.dp)
 
     }
 }
 
+private val oliveGreen = Color(0xFF808000)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(title : String) {
+
     TopAppBar(
         title = { Text(text = title , color = Color.Black) },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    Color = Color.Black
+                )
+            }
+        },
+
+
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.LightGray // sets the background color
+            containerColor = oliveGreen // sets the background color
         ),
         modifier = Modifier.fillMaxWidth()
     )
