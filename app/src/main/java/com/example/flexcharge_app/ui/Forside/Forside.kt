3a2102552d0330a4ad30bc053a1cb90da9e3c151
AsgerFrandsen <asgerfrandsen@gmail.com>
2024-10-31 15:22:37 +0100

    Forside package

---
+++ app/src/main/java/com/example/flexcharge_app/ui/Forside/Forside.kt
@@ -0,0 +1,119 @@
+package com.example.flexcharge_app.ui.Forside
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.flexcharge_app.R
+import com.example.flexcharge_app.ui.theme.FLEXCHARGEappTheme
+
+@Composable
+fun ForsideScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))
+            .padding(vertical = 16.dp)
+    ) {
+        Text(
+            text = "Support",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 28.sp),
+            color = Color.Black,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.flexcharger_image),
+            contentDescription = "Flexcharge Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 0.dp)
+                .height(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Info: Vælg et af punkterne",
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp),
+            color = Color.Black,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ButtonRow("Er du ny kunde?", horizontalPadding = 0.dp)
+        ButtonRow("Har problemer med din ladestation?", horizontalPadding = 0.dp)
+        ButtonRow("Har problemer med betaling af opladning?", horizontalPadding = 0.dp)
+        ButtonRow("Har du andre problemer?", horizontalPadding = 0.dp)
+    }
+}
+
+@Composable
+fun ButtonRow(text: String, horizontalPadding: Dp, showArrow: Boolean = true) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = horizontalPadding, vertical = 8.dp)
+            .height(56.dp)
+            .background(Color.White, RoundedCornerShape(16.dp))
+            .padding(start = 16.dp)
+            .clickable { /* Handle button click */ }
+    ) {
+        Text(
+            text = text,
+            color = Color.Black,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Start,
+            modifier = Modifier.weight(1f)
+        )
+        if (showArrow) {
+            Icon(
+                painter = painterResource(id = R.drawable.button_arrow),
+                contentDescription = null,
+                tint = Color.Black,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewForsideScreen() {
+    FLEXCHARGEappTheme {
+        ForsideScreen()
+    }
+}
